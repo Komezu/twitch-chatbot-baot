ab63a936d7b2b15922011ecb06bd87be94d5096c
Jin Qi <jenny_qiqu@hotmail.com>
2023-01-28 01:19:26 -0500

    Added join sound when user joins stream

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const tmi = require('tmi.js');
+const sound = require('sound-play');
 const { BLOCKED_WORDS } = require('./constants');
 
 const options = {
@@ -16,12 +17,24 @@ const client = new tmi.Client(options);
 
 client.connect();
 
+client.on('join', (channel, username, self) => {
+  // Ignore bot joining
+  if (self) return;
+
+  // Play chat joining sound
+  playJoinSound(username);
+});
+
 client.on('message', (channel, userstate, message, self) => {
 	// Ignore echoed messages
 	if(self) return;
 
   // Allow bypass of blocked words for bot itself
   if (userstate.username === process.env.BOT_USERNAME) return;
+
+  if (message === '!bot') {
+    client.say(channel, 'Bot is here!');
+  }
 
   // Monitor and delete messages with blocked words
   checkTwitchChat(channel, userstate, message);
@@ -34,8 +47,17 @@ function checkTwitchChat(channel, userstate, message) {
 
   if (shouldDeleteMessage) {
     // Delete message
-    client.deletemessage(channel, userstate.id);
-    // Tell user message was deleted
-    client.say(channel, `@${userstate.username}, sorry, your message was deleted.`);
+    client.deletemessage(channel, userstate.id)
+      .then(() => {
+        // Tell user message was deleted
+        client.say(channel, `Sorry @${userstate.username}, your message was deleted. Don't say bad words!`);
+      })
+      .catch(err => console.log(err));
   }
+}
+
+function playJoinSound(username) {
+  sound.play(`${__dirname}/../sound-files/magic-mallet.mp3`)
+    .then(() => console.log(username))
+    .catch(err => console.log(err));
 }
