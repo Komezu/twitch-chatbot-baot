6ea756289c5b8207310a8f091453fff29f91e1bf
Jin Qi <jenny_qiqu@hotmail.com>
2023-01-31 05:40:40 -0500

    Added helpers to get summary with game command

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,8 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 import sound from 'sound-play';
-import { BLOCKED_WORDS, COMMANDS } from './constants.js';
+import axios from 'axios';
+import { BLOCKED_WORDS } from './constants.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -17,4 +18,56 @@ export function playFirstMessageSound() {
 export function containsBlockedWord(message) {
   message = message.toLowerCase();
   return BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord));
+}
+
+export async function getGameName(channel) {
+  // Call Twitch Helix API streams endpoint
+  const url = `https://api.twitch.tv/helix/streams?type=live&user_login=${channel.substring(1)}`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OAUTH_TOKEN}`,
+        'Client-Id': process.env.CLIENT_ID
+      }
+    });
+    return response.data.data[0].game_name;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+export async function getIGDBId(game) {
+  // Call Twitch Helix API games endpoint
+  const url = `https://api.twitch.tv/helix/games?name=${game}`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OAUTH_TOKEN}`,
+        'Client-Id': process.env.CLIENT_ID
+      }
+    });
+    return response.data.data[0].igdb_id;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+export async function getGameSummary(id) {
+  // Call IGDB API games endpoint
+  const url = 'https://api.igdb.com/v4/games';
+  const body = `fields summary; where id = ${id};`;
+
+  try {
+    const response = await axios.post(url, body, {
+      headers: {
+        'Authorization': `Bearer ${process.env.IGDB_TOKEN}`,
+        'Client-ID': process.env.CLIENT_ID
+      }
+    });
+    return response.data[0].summary;
+  } catch(err) {
+    console.log(err);
+  }
 }
